5880414fccfab027cb651eefa75b1fb09acf6b72
lx4556332 <lx4556332@126.com>
2020-03-12 23:58:55 +0800

    Client Credentials

---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var dicso = await client.GetDiscoveryDocumentAsync("http://localhost:5000/");
+
+            if (dicso.IsError)
+            {
+                Console.WriteLine(dicso.Error);
+                return;
+            }
+
+            //request access token 
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address=dicso.TokenEndpoint,
+                ClientId= "client",
+                ClientSecret= "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scope= "api1 openid"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync(dicso.UserInfoEndpoint);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
