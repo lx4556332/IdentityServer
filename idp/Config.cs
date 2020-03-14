6399f9cf04fe08144b73370843b0c03146f7562d
lx4556332 <lx4556332@126.com>
2020-03-14 13:42:43 +0800

    用户名和密码

--- idp/Config.cs
+++ idp/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 using static IdentityModel.OidcConstants;
@@ -39,6 +40,15 @@ namespace idp
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
                     AllowedScopes = { "api1" }
+                },
+                new Client{
+                 ClientId="wpf client",
+                 AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                 ClientSecrets={ new Secret("wpf secrect".Sha256())},
+                 AllowedScopes={ "api1",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
     }
