b95c8e7469ec9a907ebc2ab44f095b0446d5a697
lx4556332 <lx4556332@126.com>
2020-03-15 10:38:38 +0800

    Code Flow 实例

---
+++ MvcClient/MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using MvcClient.Models;
+
+namespace MvcClient.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var IdToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+            var refreshToken=await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            var authorizationCode=await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.Code);
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
